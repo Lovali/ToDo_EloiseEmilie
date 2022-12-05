944f22f2349f126cd9c0c6215d4e6875ef4d614e
Eloïse Bonnet <eloise.bonnet@telecom-sudparis.eu>
2022-12-05 15:45:14 +0100

    TP3 Q11

--- app/src/main/java/com/eloemi/todo/data/TasksWebService.kt
+++ app/src/main/java/com/eloemi/todo/data/TasksWebService.kt
@@ -2,9 +2,18 @@ package com.eloemi.todo.data
 
 import com.eloemi.todo.tasklist.Task
 import retrofit2.Response
-import retrofit2.http.GET
+import retrofit2.http.*
 
 interface TasksWebService {
     @GET("/rest/v2/tasks/")
     suspend fun fetchTasks(): Response<List<Task>>
+
+    @POST("/rest/v2/tasks/")
+    suspend fun create(@Body task: Task): Response<Task>
+
+    @POST("/rest/v2/tasks/{id}")
+    suspend fun update(@Body task: Task, @Path("id") id: String = task.id): Response<Task>
+
+    @DELETE("/rest/v2/tasks/{id}")
+    suspend fun delete(@Body task: Task, @Path("id") id:String = task.id) : Response<Task>
 }
